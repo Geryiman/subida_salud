0610273e4b523e5cbfe7b6cab9b6d1c3ddb4e225
Geryiman <geryiman0@gmail.com>
2025-01-29 06:09:51 -0600

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -13,10 +13,14 @@ const app = express();
 // Middlewares
 app.use(cors());
 app.use(express.json());
-
+app.use(cors({
+    origin: 'https://saludfront6.vercel.app/', // Reemplaza con tu dominio de Vercel
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true, // Habilita el uso de cookies o tokens en solicitudes cruzadas
+  }));
 // Rutas
 app.use('/api/auth', authRoutes);
-app.use('/api/users', userRoutes);
+app.use('/api/users', userRoutes);AC
 app.use('/api/treatments', treatmentRoutes);
 
 // Servidor en el puerto 5000
