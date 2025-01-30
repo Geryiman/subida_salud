37e8134f2d6d92f8a98d5d28138e7d2271629ebb
Geryiman <geryiman0@gmail.com>
2025-01-30 07:15:28 -0600

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -10,22 +10,22 @@ const treatmentRoutes = require('./routes/treatmentRoutes');
 
 const app = express();
 
-// Configuración de CORS con variable de entorno
-const FRONTEND_URL = process.env.FRONTEND_URL || 'http://localhost:3000';
-
-app.use(cors({
-    origin: FRONTEND_URL, // Se obtiene de la variable de entorno
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    credentials: true, // Permite el uso de cookies o tokens en solicitudes cruzadas
-}));
-
+// Middlewares
+app.use(cors());
 app.use(express.json());
-
+app.use(cors({
+    origin: 'https://saludfront6.vercel.app/', // Reemplaza con tu dominio de Vercel
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true, // Habilita el uso de cookies o tokens en solicitudes cruzadas
+  }));
 // Rutas
-app.use('/api/auth', authRoutes);
+app.get("/", (req, ress) => [
+    res.send("Hola Mundo")
+])
+app.use('/api/auth', authRoutes); 
 app.use('/api/users', userRoutes);
 app.use('/api/treatments', treatmentRoutes);
 
-// Servidor en el puerto definido en variable de entorno o 5000 por defecto
+// Servidor en el puerto 5000
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Servidor corriendo en http://localhost:${PORT}`));
