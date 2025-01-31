afae50883caaa7f2e6e877198d869f97ac049e89
Geryiman <geryiman0@gmail.com>
2025-01-31 15:06:54 -0600

    Update authMiddleware.js

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,24 +1,6 @@
-const jwt = require('jsonwebtoken');
-
 const authenticateToken = (req, res, next) => {
-  const authHeader = req.header('Authorization');
-  if (!authHeader) {
-    return res.status(401).json({ error: 'Acceso denegado. No se proporcionó un token.' });
-  }
-
-  const token = authHeader.split(' ')[1]; // Eliminar el prefijo 'Bearer '
-
-  if (!token) {
-    return res.status(401).json({ error: 'Acceso denegado. Token no proporcionado.' });
-  }
-
-  try {
-    const verified = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = verified;
-    next();
-  } catch (err) {
-    res.status(401).json({ error: 'Token inválido o expirado.' });
-  }
+  // Simplemente pasa al siguiente middleware o controlador
+  next();
 };
 
 module.exports = authenticateToken;
