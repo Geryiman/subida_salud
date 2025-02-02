aabb100c368f8eb21e28a6ed1bbef4aa55c9a894
Geryiman <geryiman0@gmail.com>
2025-02-01 20:54:04 -0600

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ const app = express();
 
 // Middlewares
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*"); // Permitir solicitudes desde cualquier origen
+  res.header("Access-Control-Allow-Origin", req.headers.origin); // Permitir solicitudes desde cualquier origen
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
   res.header("Access-Control-Allow-Credentials", "true");
@@ -28,8 +28,11 @@ app.use((req, res, next) => {
 });
 
 app.use(cors({
-    origin: '*', // Permitir solicitudes desde cualquier origen
+    origin: (origin, callback) => {
+        callback(null, true); // Permitir solicitudes desde cualquier origen
+    },
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true, // Habilita cookies o tokens en solicitudes cruzadas
 }));
 app.use(express.json());
 
